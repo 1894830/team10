e9b1693e90b7d9ab1f7745f00ee6c51c3e278f4e
Santhosh <santoshcheera007@gmail.com>
2019-02-09 11:16:31 -0500

    order class

--- src/javaapplication2/Order.java
+++ src/javaapplication2/Order.java
@@ -5,10 +5,23 @@
  */
 package javaapplication2;
 
+import java.util.Date;
+
 /**
  *
  * @author 1894830
  */
 public class Order {
-    
+ Date date;
+ String number;
+ 
+ public void confim()
+ {
+     System.out.println("order confirm");
+     
+ }
+ public void close()
+ {
+     System.out.println("order close");
+ }
 }
