f248df43b63b5dd991e27bda9033248a30946317
Santhosh <santoshcheera007@gmail.com>
2019-02-09 11:29:11 -0500

    order class

---
+++ src/javaapplication2/NormalOrder.java
@@ -0,0 +1,30 @@
+package javaapplication2;
+
+import java.util.Date;
+
+public class NormalOrder {
+
+public class Order {
+ Date date;
+ String number;
+ 
+ public void confim()
+ {
+     System.out.println("order confirm");
+     
+ }
+ public void close()
+ {
+     System.out.println("order close");
+ }
+ public void dispatch()
+ {
+     System.out.println("dispatch");
+     
+ }
+ public void recieve()
+ {
+     System.out.println("receive");
+ }
+}}
+
