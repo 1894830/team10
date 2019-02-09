c079d8c6a4620254a2fe51a88baa8aac1d7e4d56
1894830 <1894830@E21035.CEGEPGIM.LCL>
2019-02-09 10:37:20 -0500

    order1

--- src/javaapplication2/customer.java
+++ src/javaapplication2/customer.java
@@ -5,10 +5,32 @@
  */
 package javaapplication2;
 
+import java.util.Date;
+
 /**
  *
  * @author 1894830
  */
-public class customer {
+public class customer extends superclass{
+    String name;
+    String location;
     
+    public customer( String name, String location) {
+       
+        super(name, location);
+    }  
+    void setName(String Name) {
+        name=Name ;
+    }
+
+    void setAddress(String Location) {
+         location=Location ;
+    }
+    String getName(String Name) {
+        return name ;
+    }
+
+    String getAddress(String Location ) {
+         return location;
+    }
 }
